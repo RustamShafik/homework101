edc84fc75d1d74a3a0c263648267c9b1705b4532
Rustam Shafikov <mitaleko@gmail.com>
2024-12-05 14:28:42 +0500

    second function ready

--- src/masks.py
+++ src/masks.py
@@ -1,7 +1,7 @@
 def get_mask_card_number(card_numba: int) -> str:
     """Функция принимает на вход номер карты и возвращает ее маску"""
     numba_str = str(card_numba)
-    return f"{numba_str[-16:-12]} {numba_str[-12:-10]}** **** {numba_str[-4:]}"
+    return f"{numba_str[:-12]} {numba_str[-12:-10]}** **** {numba_str[-4:]}"
 
 
 def get_mask_account(account: int) -> str:
@@ -9,6 +9,6 @@ def get_mask_account(account: int) -> str:
     account_str = str(account)
     return f"**{account_str[-4:]}"
 
-
-print(get_mask_card_number(7000792289606361))
-print(get_mask_account(73654108430135874305))
+if __name__ == "__main__":
+    print(get_mask_card_number(7000792289606361))
+    print(get_mask_account(73654108430135874305))
--- src/widget.py
+++ src/widget.py
@@ -1,2 +1,17 @@
+from masks import get_mask_card_number
 def mask_account_card(account: str) -> str:
-    '''Функция маскирует номер счета'''
+    '''Функция маскирует номер счета или карты'''
+    if account[0:4] == "Счет":
+        return f"Счет **{account[-4:]}"
+    return get_mask_card_number(account)
+
+def get_date(date: str) -> str:
+    '''Функция принимает на вход строку и форматирвует ее по условиям'''
+    return f"{date[8:10]}.{date[5:7]}.{date[0:4]}"
+
+
+if __name__ == "__main__":
+    print(mask_account_card("Visa Platinum 7000792289606361"))
+    print(mask_account_card("Maestro 7000792289606361"))
+    print(mask_account_card("Счет 73654108430135874305"))
+    print(get_date("2024-03-11T02:26:18.671407"))
