b21b993dc866621a99782d078f17597771b994af
Rustam Shafikov <mitaleko@gmail.com>
2024-12-21 11:26:18 +0500

    First two tests for masks.py functions

--- tests/test_masks.py
+++ tests/test_masks.py
@@ -3,8 +3,28 @@ from src.masks import get_mask_card_number, get_mask_account
 
 
 @pytest.fixture()
-def card_numbers():
-    return ["7000 79** **** 6361", "7000 79** **** 6362", "7000 79** **** 6363"]
+def valid_card_numbers():
+    return [
+("7000794444446361", "7000 79** **** 6361"),
+("7000794444446362", "7000 79** **** 6362"),
+("7000794444446363", "7000 79** **** 6363")
+]
+
+def test_get_mask_card_number(valid_card_numbers):
+    for card_numba, expected in valid_card_numbers:
+        assert get_mask_card_number(card_numba) == expected
+
+@pytest.fixture()
+def valid_account_numbers():
+    return [
+("73654108430135874305", "**4305"),
+("73654108430135674375", "**4375"),
+("73786896930135671111", "**1111")
+]
+
+def test_get_account_number(valid_account_numbers):
+    for account_numba, expected in valid_account_numbers:
+        assert get_mask_account(account_numba) == expected
 
 @pytest.mark.parametrize("card_numba, expected", [("7000794444446361", "7000 79** **** 6361"),
                                                   ("7000794444446362", "7000 79** **** 6362"),
@@ -18,6 +38,21 @@ def test_get_mask_card_number(card_numba, expected):
 def test_get_mask_account(account, expected):
     assert get_mask_account(account) == expected
 
+
+def test_get_mask_card_incorrect_length():
+    with pytest.raises(ValueError) as exc_info:
+        get_mask_card_number("1")
+
+    # Проверяем, что сообщение об ошибке соответствует ожидаемому
+    assert str(exc_info.value) == "Номер карты должен содержать 16 символов"
+
+
+def test_get_mask_account_not_string():
+    with pytest.raises(ValueError) as exc_info:
+        get_mask_account(156464564564564564564564568999999999)
+
+    # Проверяем, что сообщение об ошибке соответствует ожидаемому
+    assert str(exc_info.value) == "Номер счета должен представлять собой строку"
 
 
 
