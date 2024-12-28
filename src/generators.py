f7a88b5b1358846fcdf4d0827c211e269dee8f5f
Rustam Shafikov <mitaleko@gmail.com>
2024-12-28 17:56:59 +0500

    Three functions ready and working

--- src/generators.py
+++ src/generators.py
@@ -1,4 +1,4 @@
-from typing import List, Dict, Iterator
+from typing import Dict, Iterator
 
 
 def filter_by_currency(transactions: list[dict], currency: str) -> Iterator[Dict]:
@@ -7,14 +7,17 @@ def filter_by_currency(transactions: list[dict], currency: str) -> Iterator[Dict
         if item["operationAmount"]["currency"]["name"] == currency:
             yield item
 
-def transaction_descriptions (transactions: list[dict]) -> Iterator[str]:
+
+def transaction_descriptions(transactions: list[dict]) -> Iterator[str]:
     """Функция извлекает описание транзакций"""
     for item in transactions:
         yield item["description"]
+
 
 def card_number_generator(start: int, finish: int) -> Iterator[str]:
     """Функция генерирует номера карт с разделением по 4 цифры"""
     for x in range(start, finish+1):
         formatted_number = f"{x:016d}"
-        formatted_number_with_spaces = ' '.join([formatted_number[i:i + 4] for i in range(0, len(formatted_number), 4)])
+        formatted_number_with_spaces = ' '.join([formatted_number[i:i + 4]
+                                                 for i in range(0, len(formatted_number), 4)])
         yield formatted_number_with_spaces
