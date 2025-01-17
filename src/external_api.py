f238ea9880253a73674953fbe01333f60a87abdc
Rustam Shafikov <mitaleko@gmail.com>
2025-01-17 13:04:17 +0500

    Function convert_op transaction) ready

---
+++ src/external_api.py
@@ -0,0 +1,26 @@
+import os
+from dotenv import load_dotenv
+import requests
+import json
+
+load_dotenv()
+API_KEY = os.getenv('API_KEY')
+
+def convert_op(transaction):
+    '''Функция принимает транзакцию в виде словаря {"amount": 150.0, "currency": "EUR"} и возвращает сумму транзакции в рублях'''
+    trans_currency = transaction["currency"]
+    url = f"https://api.apilayer.com/exchangerates_data/latest?symbols=RUB&base={trans_currency}"
+
+    headers = {
+        "apikey": API_KEY
+    }
+
+    response = requests.request("GET", url, headers=headers)
+
+    status_code = response.status_code
+    if status_code != 200:
+        return("Произошла ошибка")
+    result = response.json()
+    return (transaction["amount"] * result["rates"]["RUB"])
+
+
